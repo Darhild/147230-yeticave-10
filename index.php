d9176215ba39b895d308448d4a4d017d215a7fd3
Darhi <isaeva-mariya@yandex.ru>
2019-08-15 00:24:59 +0300

    Удалены лишние переменные

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once("functions.php");
 $header = include_template("header.php", ["is_auth" => $is_auth, "user_name" => $user_name]);
 $footer = include_template("footer.php", ["categories" => $categories]);
 $page_content = include_template("main.php", ["categories" => $categories, "lots" => $lots]);
-$layout_content = include_template("layout.php", ["is_auth" => $is_auth, "user_name" => $user_name, "page_title" => "Интернет-аукцион горнолыжного снаряжения", "header" => $header, "footer" => $footer, "content" => $page_content, "categories" => $categories]);
+$layout_content = include_template("layout.php", ["page_title" => "Интернет-аукцион горнолыжного снаряжения", "header" => $header, "footer" => $footer, "content" => $page_content, "categories" => $categories]);
 
 print($layout_content);
 ?>
