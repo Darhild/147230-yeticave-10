81ae9a94fbe2334a36bd70c2fd0ac3c044c03378
Darhi <isaeva-mariya@yandex.ru>
2019-08-20 23:56:50 +0300

    Поправлена схема БД

--- schema.sql
+++ schema.sql
@@ -1,42 +1,39 @@
-CREATE DATABASE yeticave
+CREATE DATABASE `47230-yeticave-10`
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
-USE yeticave;
+USE `47230-yeticave-10`;
 CREATE TABLE category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name CHAR(255) NOT NULL UNIQUE,
-    symbol_code CHAR(128) NOT NULL
+    symbol_code CHAR(64) NOT NULL
 );
 CREATE TABLE lot (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    date_create DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name CHAR(255) NOT NULL,
     description CHAR(255) NOT NULL,
-    image CHAR(255) NOT NULL,
-    start_price DECIMAL NOT NULL,
-    delete_date DATETIME NOT NULL,
-    rate_step DECIMAL NOT NULL,
-    user_id INT NOT NULL,
-    winner_id INT,
+    image_url CHAR(255) NOT NULL,
+    start_price DECIMAL(10,2) NOT NULL,
+    date_expire DATETIME NOT NULL,
+    bid_step DECIMAL NOT NULL,
+    seller_id INT NOT NULL,
+    winner_id INT DEFAULT NULL,
     category_id INT NOT NULL
 );
-CREATE TABLE rate (
+CREATE TABLE bid (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    sum DECIMAL NOT NULL,
+    date_create DATETIME DEFAULT CURRENT_TIMESTAMP,
+    value INT NOT NULL,
     user_id INT NOT NULL,
     lot_id INT NOT NULL
 );
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    create_account_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    date_create DATETIME DEFAULT CURRENT_TIMESTAMP,
     email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255) NOT NULL UNIQUE,
+    name CHAR(255) NOT NULL,
     password CHAR(64) NOT NULL,
-    avatar CHAR(255),
-    contacts TEXT NOT NULL,
-    lot_id INT,
-    rate_id INT
+    contacts TEXT NOT NULL
 );
 CREATE INDEX lot_name ON lot(name);
 CREATE INDEX lot_desc ON lot(description);
