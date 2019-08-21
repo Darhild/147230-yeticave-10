4a4b5643f1edab287750adfaf728d3286b113b98
Исаева Мария <isaeva-mariya@yandex.ru>
2019-08-21 11:42:27 +0300

    Поправлена схема БД

--- schema.sql
+++ schema.sql
@@ -10,9 +10,9 @@ CREATE TABLE category (
 CREATE TABLE lot (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date_create DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name CHAR(255) NOT NULL,
-    description CHAR(255) NOT NULL,
-    image_url CHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
     start_price DECIMAL(10,2) NOT NULL,
     date_expire DATETIME NOT NULL,
     bid_step DECIMAL NOT NULL,
@@ -30,10 +30,34 @@ CREATE TABLE bid (
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date_create DATETIME DEFAULT CURRENT_TIMESTAMP,
-    email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
     password CHAR(64) NOT NULL,
     contacts TEXT NOT NULL
 );
 CREATE INDEX lot_name ON lot(name);
 CREATE INDEX lot_desc ON lot(description);
+CREATE INDEX fk_idx_seller ON lot(seller_id ASC);
+CREATE INDEX fk_idx_user ON bid(user_id ASC);
+CREATE INDEX fk_idx_lot ON bid(lot_id ASC);
+
+ALTER TABLE lot 
+ADD CONSTRAINT fk_lot_user
+    FOREIGN KEY (seller_id)
+    REFERENCES user(id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE;
+
+ALTER TABLE bid 
+ADD CONSTRAINT fk_bid_user
+    FOREIGN KEY (user_id)
+    REFERENCES user(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
+
+ALTER TABLE bid 
+ADD CONSTRAINT fk_bid_lot
+    FOREIGN KEY (lot_id)
+    REFERENCES lot(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
